29617120bbe9464f948f0b57d3251c6c336eae8f
Open_MyTrust_profile <clementbtc2023@gmail.com>
2026-06-01 14:25:36 +0100

    Update app.js

--- app.js
+++ app.js
@@ -66,67 +66,7 @@ loadTransactions();
 }
 
 }
+  
 );
 
-const themeToggle =
-document.getElementById("themeToggle");
-
-const appTheme =
-document.querySelector(".appTheme");
-
-/* DEFAULT THEME */
-
-appTheme.classList.add("dark");
-
-themeToggle.addEventListener(
-"click",
-function(e){
-
-/* TOGGLE THEMES */
-
-if(appTheme.classList.contains("dark")){
-
-appTheme.classList.remove("dark");
-appTheme.classList.add("light");
-
-}else{
-
-appTheme.classList.remove("light");
-appTheme.classList.add("dark");
-
 }
-
-/* RIPPLE EFFECT */
-
-const ripple =
-document.createElement("span");
-
-ripple.classList.add("ripple");
-
-const rect =
-this.getBoundingClientRect();
-
-const size =
-Math.max(rect.width, rect.height);
-
-ripple.style.width =
-size + "px";
-
-ripple.style.height =
-size + "px";
-
-ripple.style.left =
-e.clientX - rect.left - size/2 + "px";
-
-ripple.style.top =
-e.clientY - rect.top - size/2 + "px";
-
-this.appendChild(ripple);
-
-setTimeout(()=>{
-
-ripple.remove();
-
-},600);
-
-});
